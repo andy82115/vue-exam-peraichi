c8bf2970f5423355af2e3f2acb9bd40ce1aa6de1
andy82115 <andy82115@gmail.com>
2025-01-10 17:06:57 +0900

    Nust始める〜〜〜〜　:tada:

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<script lang="ts"></script>
+
+<template>
+  <div>
+    <v-app>
+        <NuxtPage />
+    </v-app>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <div>
+        <v-btn>Hello World</v-btn>
+    </div>
+</template>
