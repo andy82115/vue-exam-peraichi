bff9fb29d94953f81f961d35d6a264956213175d
andy82115 <andy82115@gmail.com>
2025-01-10 17:24:33 +0900

    tailwindcssを仕掛ける

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,12 @@
 </script>
 
 <template>
-    <div>
-        <v-btn>Hello World</v-btn>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <v-app>
+            <v-container>
+                <h1 class="text-4xl font-bold text-blue-500">Hello Nuxt + Vuetify + TailwindCSS!</h1>
+                <v-btn color="primary">Vuetify Button</v-btn>
+            </v-container>
+        </v-app>
     </div>
 </template>
