28a30a8afd1b5865b2b15d2a9037d7edd1214463
andy82115 <andy82115@gmail.com>
2025-01-11 16:32:16 +0900

    unit testの初期化

---
+++ tests/firsttest.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+
+describe('first test for git action', () => {
+  it('renders initial message', () => {
+    const isTwo = 1 + 1
+    expect(isTwo).toBe(2)
+  });
+});
