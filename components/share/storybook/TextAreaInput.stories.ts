3f4115c63c3b36c51393c74c0ce123cb51ba31b8
andy82115 <andy82115@gmail.com>
2025-01-13 00:24:19 +0900

    TextAreaInputを作る

---
+++ components/share/storybook/TextAreaInput.stories.ts
@@ -0,0 +1,41 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+import TextareaInput from '../TextAreaInput.vue';
+
+const meta: Meta<typeof TextareaInput> = {
+  title: 'Components/TextareaInput', 
+  component: TextareaInput,
+  argTypes: {
+    modelValue: {
+      description: 'The value of the textarea (used with v-model)',
+      control: { type: 'text' },
+    },
+    placeholder: {
+      description: 'The label text displayed above the textarea',
+      control: { type: 'text' }, 
+    },
+    rules: {
+      description: 'Validation rules for the textarea',
+      control: { type: 'object' },
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'dark', value: '#333333' },
+      ],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TextareaInput>;
+
+export const Default: Story = {
+  args: {
+    modelValue: 'This is a sample text.',
+    placeholder: 'Description',
+  },
+};
--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,12 @@
   import TextInput from '../components/share/TextInput.vue';
   import SelectInput from '../components/share/SelectInput.vue';
   import DateInput from '../components/share/DateInput.vue';
+  import TextAreaInput from '../components/share/TextAreaInput.vue';
 
   const config = useRuntimeConfig();
   const inputValue = ref('');
   const dateInputValue = ref('');
+  const textAreaInputValue = ref('');
   const selected = ref()
 
   console.log(config.public.apiUrl);
@@ -20,5 +22,6 @@
     <TextInput v-model="inputValue" label="Enter your name" />
     <SelectInput v-model="selected" label="hellow" :options="['a','b','c','d','e']" />
     <DateInput v-model="dateInputValue" label="input date"></DateInput>
+    <TextAreaInput v-model="textAreaInputValue" placeholder="input something"></TextAreaInput>
   </v-col>
 </template>
