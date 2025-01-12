7050eeda43dd3c120fe11663ba04f60328696cef
andy82115 <andy82115@gmail.com>
2025-01-12 22:33:31 +0900

    TextInputのUIを作る

--- components/share/FormLabel.vue
+++ components/share/FormLabel.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="w-full h-fit flex flex-row justify-center items-center">
-    <label class="text-sm font-bold text-gray-500 md:mr-2">
+  <div class="w-full h-fit flex flex-col justify-center items-center">
+    <label class="text-sm font-extrabold text-gray-500 md:mr-2">
       {{ labelText }}
     </label>
-    <div class="w-4 h-4"></div>
+    <div class="w-2 h-2"></div>
     <label
       v-if="labelRequired"
-      class="text-sm font-bold text-blue-600 md:mr-2 bg-red-600 rounded-lg p-3 m-2"
+      class="text-sm font-bold text-white md:mr-2 bg-red-600 rounded-lg px-3 py-1 m-2"
     >
       必須
     </label>
     <label
       v-else
-      class="text-sm font-bold text-pink-600 md:mr-2 bg-gray-600 rounded-lg p-3 m-2"
+      class="text-sm font-bold text-black md:mr-2 bg-gray-400 rounded-lg px-3 py-1 p-3 m-2"
     >
       任意
     </label>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,18 @@
-<script lang="ts" setup>
+<script setup lang="ts">
+  import TextInput from '../components/share/TextInput.vue';
+
   const config = useRuntimeConfig();
+  const inputValue = ref('');
+
   console.log(config.public.apiUrl);
 </script>
 
 <template>
-  <div
-    class="flex flex-col items-center justify-center min-h-screen bg-gray-100"
-  >
-    <v-app>
-      <v-container>
-        <h1 class="text-4xl font-bold text-blue-500">
-          Hello Nuxt + Vuetify + TailwindCSS!
-        </h1>
-        <v-btn color="primary">Vuetify Button</v-btn>
-      </v-container>
-    </v-app>
-  </div>
+  <v-col>
+    <h1 class="text-4xl font-bold text-blue-500">
+      Hello Nuxt + Vuetify + TailwindCSS!
+    </h1>
+    <v-btn color="primary">Vuetify Button</v-btn>
+    <TextInput v-model="inputValue" label="Enter your name" />
+  </v-col>
 </template>
